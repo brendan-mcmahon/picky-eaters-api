c7fdbb8ee88e899a5e593d31146a57a66abadefc
Brendan McMahon <brendanjeffreymcmahon@gmail.com>
2019-12-07 11:04:59 -0500

    test commit

---
+++ server.js
@@ -0,0 +1,75 @@
+var http = require('http');
+
+
+var server = http.createServer(function (req, res) {
+
+    const likedIt = { id: 1, name: "liked it" };
+    const hatedIt = { id: 2, name: "hated it" };
+    const madeSick = { id: 3, name: "made me sick" };
+
+    const brendan = { id: 1, name: "Brendan"};
+    const jamie = { id: 2, name: "Jamie" };
+
+    const restaurants = [
+        {
+            id: 1,
+            name: "Pizza King",
+            menuItems: [
+                {
+                    id: 1,
+                    name: "Tea",
+                    person: jamie,
+                    opinion: hatedIt
+                },
+                {
+                    id: 3,
+                    name: "Pepperoni Breadsticks",
+                    person: brendan,
+                    opinion: madeSick
+                }
+            ]
+        },
+        {
+            id: 2,
+            name: "Hana",
+            menuItems: [
+                {
+                    id: 2,
+                    name: "Seafood Pizza",
+                    person: jamie,
+                    opinion: likedIt
+                },
+                {
+                    id: 3,
+                    name: "Seafood Pizza",
+                    person: brendan,
+                    opinion: hatedIt
+                }
+            ]
+        },
+    ]
+
+    if(req.url == '/restaurants') {
+        res.writeHead(200, { 'Content-Type': 'application/json'});
+        res.write(JSON.stringify(restaurants));
+        res.end();
+    }
+    if (req.url == '/people') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify([brendan, jamie]));  
+        res.end(); 
+    
+    }
+    if(req.url == '/opinionOptions') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify([likedIt, hatedIt, madeSick]));  
+        res.end(); 
+    }
+    else
+        res.end('Invalid Request!');
+
+});
+
+server.listen(5000);
+
+console.log('Node.js web server at port 5000 is running..')
