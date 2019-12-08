3198f733e2a9da30f74684fb928e246d60ed43bb
Brendan McMahon <brendanjeffreymcmahon@gmail.com>
2019-12-08 01:24:11 -0500

    add port environment variable

--- server.js
+++ server.js
@@ -70,6 +70,6 @@ var server = http.createServer(function (req, res) {
 
 });
 
-server.listen(5000);
+server.listen(process.env.PORT || 5000);
 
 console.log('Node.js web server at port 5000 is running..')
