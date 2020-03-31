b6ff1d5267752b98949240e70d7457db989ff6ee
Brendan McMahon <brendanjeffreymcmahon@gmail.com>
2020-03-31 01:33:43 -0400

    switch to firebase

--- server.js
+++ server.js
@@ -1,107 +1,138 @@
 'use strict'
 
 const admin = require('firebase-admin');
-const models = require('./models/index');
 const express = require('express');
 const bodyParser = require('body-parser');
-// const { Pool } = require('pg');
-
-// const pool = new pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: true
-// });
-
 const app = express();
-
 let serviceAccount = require('./serviceAccountKey.json');
 
-const likedIt = { id: 1, name: "liked it" };
-const hatedIt = { id: 2, name: "hated it" };
-const madeSick = { id: 3, name: "made me sick" };
-
-const brendan = { id: 1, name: "Brendan"};
-const jamie = { id: 2, name: "Jamie" };
-
-const restaurants = [
-    {
-        id: 1,
-        name: "Pizza King",
-        menuItems: [
-            {
-                id: 1,
-                name: "Tea",
-                person: jamie,
-                opinion: hatedIt
-            },
-            {
-                id: 3,
-                name: "Pepperoni Breadsticks",
-                person: brendan,
-                opinion: madeSick
-            }
-        ]
-    },
-    {
-        id: 2,
-        name: "Hana",
-        menuItems: [
-            {
-                id: 2,
-                name: "Seafood Pizza",
-                person: jamie,
-                opinion: likedIt
-            },
-            {
-                id: 3,
-                name: "Seafood Pizza",
-                person: brendan,
-                opinion: hatedIt
-            }
-        ]
-    },
-]
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+  
+let db = admin.firestore();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/restaurants', (req, res) => {
-    res.set('Content-Type', 'application/json');
-    res.send(JSON.stringify(restaurants));
+    
+    console.log('Processing GET request for Restaurants');
+
+    db.collection('restaurants').get()
+    .then((snapshot) => {
+        const restaurants = [];
+        res.set('Content-Type', 'application/json');
+        snapshot.forEach((doc) => {
+            restaurants.push({
+                name: doc.data().name,
+                id: doc.id
+            });
+        });        
+        return res.send(restaurants);
+    })
+    .catch((err) => {
+        console.log('Error getting restaurants:', err);
+    });
+})
+
+app.post('/restaurant', async (req, res) => {
+
+    console.log(req.body);
+
+    let setDoc = db.collection('restaurants').doc().set(req.body);
+
+    try {
+        const response = await setDoc;
+        console.log('Set: ', response);
+        res.set('Content-Type', 'text/plain');
+        res.send(req.body);
+    }
+    catch (err) {
+        console.log(`"Add failed with error: ${err}`);
+    }
 })
 
 app.get('/people', (req, res) => {
-    res.set('Content-Type', 'application/json');
-    res.send(JSON.stringify([brendan, jamie]));  
+    console.log('Processing GET request for People');
+
+    db.collection('people').get()
+    .then((snapshot) => {
+        const people = [];
+        res.set('Content-Type', 'application/json');
+        snapshot.forEach((doc) => {
+            console.log(doc.id);
+            people.push(doc.data());
+        });        
+        return res.send(people);
+    })
+    .catch((err) => {
+        console.log('Error getting people:', err);
+    });
 })
 
-app.get('/opinionOptions', (req, res) => {
-    res.set('Content-Type', 'application/json');
-    res.send(JSON.stringify([likedIt, hatedIt, madeSick]));  
+app.post('/person', async (req, res) => {
+
+    console.log(req.body);
+
+    let setDoc = db.collection('people').doc().set(req.body);
+
+    try {
+        const response = await setDoc;
+        console.log('Set: ', response);
+        res.set('Content-Type', 'text/plain');
+        console.log(`new person: ${response.name}`);
+        res.send(req.body);
+    }
+    catch (err) {
+        console.log(`"Add failed with error: ${err}`);
+    }
 })
 
-app.post('/addMenuItem', (req, res) => {
+app.get('/reviews/:restaurantId', (req, res) => {
+    console.log(`Processing GET request for Reviews for restaurant id: ${req.params["restaurantId"]}`);
 
-    const restaurantId = req.body.restaurantId;
-    const personName = req.body.menuItem.person.name;
-    const opinionId = req.body.menuItem.opinion;
-    const itemName = req.body.menuItem.name;
+    db.collection('reviews')
+    .where('restaurantId', '==', req.params["restaurantId"])
+    .get()
+    .then((snapshot) => {
 
-    let stringified = '';
-    switch(opinionId){
-        case "LikedIt":
-            stringified =  `${personName} likes the ${itemName}.`;
-            break;
-        case "MadeSick":
-            stringified =  `the ${itemName} made ${personName} sick!`;
-            break;
-        case "HatedIt":
-            stringified =  `${personName} did not care for the ${itemName}.`;
-            break;
-    }    
+        if (snapshot.empty){
+            return res.status(404).send("No reviews found");
+        }
 
-    console.log(stringified);
-    res.set('Content-Type', 'text/plain');
-    res.send(stringified);
+        const reviews = [];
+        snapshot.forEach((doc) => {
+            reviews.push(doc.data());
+        });
+        
+        res.set('Content-Type', 'application/json');
+        return res.send(reviews);
+    })
+    .catch((err) => {
+        console.log('Error getting reviews:', err);
+    });
+})
+
+app.post('/reviews', async (req, res) => {
+
+    const personName = req.body.person;
+    const opinionId = req.body.orderAgain;
+    const itemName = req.body.name;
+
+    let stringified = `${personName} would${opinionId ? '' : ' not'} order ${itemName} again.`;
+
+    let setDoc = db.collection('reviews').doc().set(req.body);
+
+    try {
+        const response = await setDoc;
+        console.log('Set: ', response);
+        res.set('Content-Type', 'text/plain');
+        res.send(stringified);
+    }
+    catch (err) {
+        console.log(`"Add failed with error: ${err}`);
+    }
 })
 
 app.listen(process.env.PORT || 5000, err => {
