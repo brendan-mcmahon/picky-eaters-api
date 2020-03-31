ff855a2c65642ee795086c602b9246e74d3522ae
Brendan McMahon <brendanjeffreymcmahon@gmail.com>
2020-03-31 18:52:56 -0400

    Use environment vars for serviceAccountKey.json

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const bodyParser = require('body-parser');
 const app = express();
 let serviceAccount = require('./serviceAccountKey.json');
 
+serviceAccount.project_id = process.env.FIREBASE_PROJECT_ID;
+serviceAccount.private_key_id = process.env.FIREBASE_PRIVATE_KEY_ID;
+serviceAccount.private_key = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
+serviceAccount.client_email = process.env.FIREBASE_CLIENT_EMAIL;
+serviceAccount.client_id = process.env.FIREBASE_CLIENT_ID;
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
